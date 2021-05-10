b2b13f538ea551ca82c3354f270d827cd18d7d44
nowapracownia <tomasz.nowak@nowapracownia.pl>
2021-05-10 21:56:09 +0200

    Header behavior, flags, styling tweaks

--- js/theme-scripts.js
+++ js/theme-scripts.js
@@ -7,8 +7,21 @@
             console.log(thisAnchor);
             scrollToAnchor(thisAnchor);
         });
-    })
-
+        $(window).scroll(function() {
+            let fixedCheck = $('.site-header').hasClass('menu-fixed');
+            let scroll = $(window).scrollTop();
+            if(scroll > 200) {
+                if(!fixedCheck) {
+                    $('.site-header').addClass('menu-fixed');
+                }
+            }
+            else {
+                if(fixedCheck) {
+                    $('.site-header').removeClass('menu-fixed');
+                }
+            }
+        });
+    });
     function scrollToAnchor(aid){
         var aTag = $("a[name='"+ aid +"']");
         $('html,body').animate({scrollTop: aTag.offset().top},'slow');
